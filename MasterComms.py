dd23b69f352c2917b3c0f6169bc928a028fc5f81
gh-gill <62687520+gh-gill@users.noreply.github.com>
2025-08-26 09:48:58 +0100

    Update MasterComms.py

--- MasterComms.py
+++ MasterComms.py
@@ -2,19 +2,11 @@ from umodbus.serial import Serial as ModbusRTUMaster
 
 # RTU Host/Master setup
 
-# the following definition is for an ESP32
-rtu_pins = (25, 26)         # (TX, RX)
-uart_id = 1
-
 # the following definition is for a RP2
-# rtu_pins = (Pin(0), Pin(1))     # (TX, RX)
-# uart_id = 0
+rtu_pins = (Pin(0), Pin(1))     # (TX, RX)
+uart_id = 0
 #
 # rtu_pins = (Pin(4), Pin(5))     # (TX, RX)
-# uart_id = 1
-
-# the following definition is for a pyboard
-# rtu_pins = (Pin(PB6), Pin(PB7))   # (TX, RX)
 # uart_id = 1
 
 host = ModbusRTUMaster(
@@ -27,5 +19,39 @@ host = ModbusRTUMaster(
     uart_id=uart_id         # optional, default 1, see port specific documentation
 )
 
+#Supported Modbus functions
+# ID    Description
+# 1     Read coils 
+# 2     Read discrete inputs
+# 3     Read holding registers
+# 4     Read input registers
+# 5     Write single coil
+# 6     Write single register
+# 15    Write multiple coils
+# 16    Write multiple registers
+
+# 0x01 read_coils
+# 0x02 read_discrete_inputs
+# 0x03 read_holding_registers
+# 0x04 read_input_registers
+# 0x05 write_single_coil
+# 0x06 write_single_register
+# 0x0F write_multiple_coils
+# 0x10 write_multiple_registers
+
+# reading one coil returns a list of 1 boolean element
+# host.read_coils(slave_addr=10, starting_addr=123, coil_qty=1)
+# [True]
+# reading 3 coils returns a list of 3 boolean elements
+# host.read_coils(slave_addr=10, starting_addr=126, coil_qty=3)
+# [False, True, False]
+
 coil_status = host.read_coils(slave_addr=10, starting_addr=123, coil_qty=1)
 print('Status of coil 123: {}'.format(coil_status))
+time.sleep(1)
+# WRITE COILS
+new_coil_val = 0
+coil_status = host.write_single_coil(slave_addr=10, output_address=123, output_value=new_coil_val)
+print('Result of setting COIL 123: {}'.format(coil_address, operation_status))
+time.sleep(1)
+
